0b441c7b6d48674abceec26d4eea29fad09b0dd3
IvanLin24 <ivanlinaev3@gmail.com>
2025-12-12 02:44:34 +0300

    Add negative test cases for coord_to_index

--- test_battleship.py
+++ test_battleship.py
@@ -8,7 +8,7 @@ class TestBattleship(unittest.TestCase):
         self.assertEqual(len(board), 10)
         self.assertEqual(len(board[0]), 10)
         self.assertEqual(board[0][0], '~')
-
+        
     def test_can_place(self):
         board = create_board()
         self.assertTrue(can_place(board, 0, 0, 3, True))
@@ -20,6 +20,20 @@ class TestBattleship(unittest.TestCase):
         self.assertEqual(count_ships(board), 0)
         place_ship(board, 0, 0, 3, True)
         self.assertEqual(count_ships(board), 1)
+
+    def test_coord_to_index_invalid(self):
+        """Тестирование некорректных координат"""
+        from utils import coord_to_index
+        # Неверная буква
+        self.assertIsNone(coord_to_index('Л', 5))
+        # Неверное число (меньше 1)
+        self.assertIsNone(coord_to_index('А', 0))
+        # Неверное число (больше 10)
+        self.assertIsNone(coord_to_index('А', 11))
+        # Пустая строка
+        self.assertIsNone(coord_to_index('', 5))
+        # Не число (передаём строку)
+        self.assertIsNone(coord_to_index('А', 'не число'))
 
 
 if __name__ == '__main__':
