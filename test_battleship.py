1ee6481d442b33289e5fe1c9f86b31304d080b0e
IvanLin24 <ivanlinaev3@gmail.com>
2025-12-21 23:47:50 +0300

    Update test_battleship.py

--- test_battleship.py
+++ test_battleship.py
@@ -1,6 +1,6 @@
 import unittest
-from utils import create_board, can_place, place_ship, count_ships
-
+import os
+from utils import create_board, can_place, place_ship, count_ships, save_board
 
 class TestBattleship(unittest.TestCase):
     def test_create_board(self):
@@ -8,7 +8,7 @@ class TestBattleship(unittest.TestCase):
         self.assertEqual(len(board), 10)
         self.assertEqual(len(board[0]), 10)
         self.assertEqual(board[0][0], '~')
-        
+
     def test_can_place(self):
         board = create_board()
         self.assertTrue(can_place(board, 0, 0, 3, True))
@@ -34,30 +34,30 @@ class TestBattleship(unittest.TestCase):
         self.assertIsNone(coord_to_index('', 5))
         # Не число (передаём строку)
         self.assertIsNone(coord_to_index('А', 'не число'))
-        
+
     def test_save_board_raises_exceptions(self):
         """Тест на выброс исключений в save_board"""
         board = create_board()
-        
+
         # 1. Неверный тип доски (не список)
         with self.assertRaises(ValueError):
             save_board("не доска", "test.txt")
-        
+
         # 2. Доска неправильного размера
         with self.assertRaises(ValueError):
             save_board([['~'] * 10] * 8, "test.txt")  # 8 строк вместо 10
-        
+
         # 3. Строка неправильного размера
         bad_board = [['~'] * 8 for _ in range(10)]  # 8 столбцов вместо 10
         with self.assertRaises(ValueError):
             save_board(bad_board, "test.txt")
-        
+
         # 4. Недопустимый символ в доске
         bad_board = create_board()
         bad_board[0][0] = 'Z'  # Неправильный символ
         with self.assertRaises(ValueError):
             save_board(bad_board, "test.txt")
-        
+
         # 5. Ошибка записи в файл (системная папка)
         with self.assertRaises(IOError):
             if os.name == 'posix':  # Linux/Mac
